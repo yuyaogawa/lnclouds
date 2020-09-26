b5aa028cae0d102bd3b29c7107f1a206c8d09630
Yuya Ogawa <yuya.ogw@gmail.com>
2020-09-26 17:29:52 +0900

    Add ndlc

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,18 @@ router.get('/', function(req, res, next) {
     var hostname = 'vega-422';
     var json = execSync('curl -s https://bitclouds.sh/status/' + hostname).toString();
     var status = JSON.parse(json);
-    console.log(status);
+
     var json2 = execSync('curl -s https://bitclouds.sh/topup/' + hostname + '/1000').toString();
     var topup = JSON.parse(json2);
+
+    const DIR = '~/NDLC/NDLC.CLI/bin/Release/netcoreapp3.1';
+    const json3 = execSync(DIR + '/ndlc-cli -testnet --datadir Olivia oracle show "Olivia"').toString();
+    console.log(json3);
+    const oracle = JSON.parse(json3);
+
     var data = { 'status': status.hours_left,
                  'topup': topup.invoice,
+                 'oracle': oracle.Pubkey,
              };
     res.render('index', {data});
 });
