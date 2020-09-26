114251545016c3d5c29bf0abe799048d2b5641a1
Yuya Ogawa <yuya.ogw@gmail.com>
2020-09-26 15:05:39 +0900

    Add topup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var execSync = require('child_process').execSync;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var hostname = 'vega-422';
+    var json = execSync('curl -s https://bitclouds.sh/status/' + hostname).toString();
+    var status = JSON.parse(json);
+    console.log(status);
+    var json2 = execSync('curl -s https://bitclouds.sh/topup/' + hostname + '/1000').toString();
+    var topup = JSON.parse(json2);
+    var data = { 'status': status.hours_left,
+                 'topup': topup.invoice,
+             };
+    res.render('index', {data});
 });
 
 module.exports = router;
